7682a6c78785ba588bec46ca0ea921ff21347f43
PreetNagda <preetnagda34@gmail.com>
2019-01-18 23:17:37 +0530

    Remake

--- js/custom/custom_js.js
+++ js/custom/custom_js.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+    $(window).scrollTop();
         $("#divHome").fadeIn();
         $("#aboutme").on('click', function (event) {
             if (this.hash !== "") {
